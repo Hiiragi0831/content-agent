50f906bd688ddd54108ada4f1140d573c57628ab
NikitaLatyshev2022 <nikitalatyshev1994@mail.ru>
2023-05-06 18:54:32 +0300

    1. Сверстал шапку 2. Начал main, но не получилось в mixin.pug его подключить, поэтому сильно разгоняться не стал

---
+++ src/components/cases/cases-swipper.js
@@ -0,0 +1,9 @@
+    var swiper = new Swiper(".slider__cards", {
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+    pagination: {
+    el: ".swiper-pagination",
+},
+});
